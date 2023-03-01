587250c7a12e340cc0acdcaf84d7d2384d461853
Ian Barkan Ducharme <ibarkan117@gmail.com>
2023-03-01 08:38:34 -0500

    DOM manpiulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,27 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-// Selects message
-document.querySelector('.message').textContent = 'Correct Number!';
-document.querySelector('.number').textContent = 13;
+// console.log(document.querySelector('.message').textContent);
+// // Selects message
+// document.querySelector('.message').textContent = 'Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 200008;
+
+const checkGuessButton = document.querySelector('.check');
+const guessInput = document.querySelector('.guess');
+const message = document.querySelector('.message');
+const number = document.querySelector('.number');
+
+const generateNewNumber = function () {
+  const myNumber = Math.floor(Math.random() * 10);
+  return myNumber;
+};
+
+checkGuessButton.addEventListener('click', function () {
+  const input = Number(guessInput.value);
+
+  if (!guess) {
+  } else if (input === number) {
+    number.textContent = input;
+  }
+});
