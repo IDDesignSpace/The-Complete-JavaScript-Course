1e1eb29cc5a47c3e112010db51a2e201ae63864b
Ian Barkan Ducharme <ianbarkanducharme@ians-mbp.mynetworksettings.com>
2023-02-28 19:54:44 -0500

    DOM manpiulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+// Selects message
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
