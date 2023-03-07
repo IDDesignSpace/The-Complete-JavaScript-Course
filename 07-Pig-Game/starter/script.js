9758f78266e15134ba477f11e9b281248e8200a3
Ian Barkan Ducharme <ibarkan117@gmail.com>
2023-03-06 20:47:25 -0500

    Added some Elements

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+// User Rolls Dice
+//
+// Selected Elements
+const scoreElement1 = document.querySelector('#score--0');
+const scoreElement2 = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+diceElement.classList.add('hidden');
+scoreElement1.textContent = 0;
+scoreElement2.textContent = 0;
