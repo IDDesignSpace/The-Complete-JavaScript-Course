1dab29ce544e765c8a67ad4480403844436b65f6
Ian Barkan Ducharme <ibarkan117@gmail.com>
2023-03-07 10:27:50 -0500

    updated current score logic

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -8,12 +8,15 @@ const diceElement = document.querySelector('.dice');
 const newGameButton = document.querySelector('.btn--new');
 const rollButton = document.querySelector('.btn--roll');
 const holdButton = document.querySelector('.btn--hold');
-const currentScoreText = document.querySelector('.current-score');
+const currentScorePlayerOne = document.querySelector('#current--0');
+const currentScorePlayerTwo = document.querySelector('#current--1');
 const playerOneSection = document.querySelector('.player--0');
 const playerTwoSection = document.querySelector('.player--1');
+let currentScore = 0;
 let playerOneScore;
 let playerTwoScore;
 let activePlayer;
+
 // Starting Conditions
 const resetGame = function () {
   activePlayer = 1;
@@ -44,37 +47,20 @@ rollButton.addEventListener('click', function () {
   const diceRoll = Math.trunc(Math.random() * 6) + 1;
   console.log(diceRoll);
   // 2. Display dice roll
-  switch (diceRoll) {
-    case 1:
-      console.log('i am working ');
-      diceElement.classList.remove('hidden');
-      diceElement.src = 'dice-1.png';
-      break;
-    case 2:
-      diceElement.src = 'dice-2.png';
-      diceElement.classList.remove('hidden');
-      break;
-    case 3:
-      diceElement.src = 'dice-3.png';
-      diceElement.classList.remove('hidden');
-      break;
-    case 4:
-      diceElement.src = 'dice-4.png';
-      diceElement.classList.remove('hidden');
-      break;
-    case 5:
-      diceElement.src = 'dice-5.png';
-      diceElement.classList.remove('hidden');
-      break;
-    case 6:
-      diceElement.src = 'dice-6.png';
-      diceElement.classList.remove('hidden');
-      break;
-  }
+
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice-${diceRoll}.png`;
+
   // 3. Check if you rolled 1: if true change to next player
   //   return dice;
-  if (diceRoll === 1) {
-    changeActivePlayer();
+  if (diceRoll != 1) {
+    // Add dice to current score
+    currentScore += diceRoll;
+    currentScorePlayerOne.textContent = currentScore;
+  } else {
+    currentScore = 0;
+    currentScorePlayerOne.textContent = currentScore;
+    // Change the active player
   }
 });
 
