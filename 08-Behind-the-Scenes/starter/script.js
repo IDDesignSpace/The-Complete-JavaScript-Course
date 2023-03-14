61ac3c30d8a52189ec385f7eb3821659d68c8355
Ian Barkan Ducharme <ibarkan117@gmail.com>
2023-03-14 09:54:50 -0400

    Completed behind the scenes activity

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function calcAge(birthyear) {
+  const age = 2023 - birthyear;
+  console.log(firstName);
+  function printAge() {
+    let output = `You are ${age} years old and you were born on ${birthyear} `;
+
+    if (birthyear >= 1981 && birthyear <= 1996) {
+      const firstName = 'Thomas';
+      const str = `Oh you're a millenial ${firstName}`;
+      console.log(str);
+
+      // Reassigning output for the outer scope vs creating an entirely new variable with const output = "new output";
+      let output = 'new OUTPUT!';
+    }
+    console.log(output);
+  }
+
+  printAge();
+  return age;
+}
+
+const firstName = 'Ian';
+
+calcAge(1990);
